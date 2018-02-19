c51a7029de6ba1bc0c93204ec586fd9bf3221d3e
Ivan Radeljic <radeljicivan85@gmail.com>
2018-02-19 11:59:42 +0100

    Add html structure for main part in app component

--- src/App.js
+++ src/App.js
@@ -8,9 +8,11 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Routes</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">Please enter your starting and end point</p>
+        <main>
+          <div className="App-form" />
+          <div className="App-list" />
+        </main>
       </div>
     );
   }
