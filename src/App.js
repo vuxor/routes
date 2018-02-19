048639a0134825f1fb0a38f562fa7d54e94c5ca5
Ivan Radeljic <radeljicivan85@gmail.com>
2018-02-20 00:50:03 +0100

    Show storage error

--- src/App.js
+++ src/App.js
@@ -50,80 +50,86 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Routes</h1>
         </header>
-        <main className="App-main">
-          <div className="App-form">
-            <p className="App-intro">
-              Please enter your starting and end point
-            </p>
-            <LocationInput
-              placeholderText="Enter your starting point"
-              placeChanged={places => console.log(places)}
-            />
-            <LocationInput
-              placeholderText="Enter your destination"
-              placeChanged={place => console.log(place)}
-            />
-            <Link to="/something" className="App-go">
-              <span>Go</span>
-              <svg
-                fill="#fff"
-                height="24"
-                viewBox="0 0 24 24"
-                width="24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M0 0h24v24H0z" fill="none" />
-                <path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z" />
-              </svg>
-            </Link>
-          </div>
-          <div className="App-list">
-            <p className="App-intro">Your Previous Routes</p>
-            <hr />
-            <ul>
-              <li className="App-list-item" key={1}>
-                <Link to="/something" className="App-routes-link">
-                  <span>
-                    <img
-                      className="App-routes-icon"
-                      src={routeIcon}
-                      width="50px"
-                      alt="Route icon"
-                    />
+        {this.state.storageError ? (
+          <p className="App-error">
+            Your browser doesn't support local storage API
+          </p>
+        ) : (
+          <main className="App-main">
+            <div className="App-form">
+              <p className="App-intro">
+                Please enter your starting and end point
+              </p>
+              <LocationInput
+                placeholderText="Enter your starting point"
+                placeChanged={places => console.log(places)}
+              />
+              <LocationInput
+                placeholderText="Enter your destination"
+                placeChanged={place => console.log(place)}
+              />
+              <Link to="/something" className="App-go">
+                <span>Go</span>
+                <svg
+                  fill="#fff"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M0 0h24v24H0z" fill="none" />
+                  <path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z" />
+                </svg>
+              </Link>
+            </div>
+            <div className="App-list">
+              <p className="App-intro">Your Previous Routes</p>
+              <hr />
+              <ul>
+                <li className="App-list-item" key={1}>
+                  <Link to="/something" className="App-routes-link">
+                    <span>
+                      <img
+                        className="App-routes-icon"
+                        src={routeIcon}
+                        width="50px"
+                        alt="Route icon"
+                      />
+                    </span>
+                    <span className="App-list-text">
+                      Origin point<br />Destination point
+                    </span>
+                  </Link>
+                  <span className="App-delete">
+                    <a>
+                      <img src={deleteIcon} alt="Delete" />
+                    </a>
                   </span>
-                  <span className="App-list-text">
-                    Origin point<br />Destination point
+                </li>
+                <li className="App-list-item" key={2}>
+                  <Link to="/something" className="App-routes-link">
+                    <span>
+                      <img
+                        className="App-routes-icon"
+                        src={routeIcon}
+                        width="50px"
+                        alt="Route icon"
+                      />
+                    </span>
+                    <span className="App-list-text">
+                      Origin point<br />Destination point
+                    </span>
+                  </Link>
+                  <span className="App-delete">
+                    <a>
+                      <img src={deleteIcon} alt="Delete" />
+                    </a>
                   </span>
-                </Link>
-                <span className="App-delete">
-                  <a>
-                    <img src={deleteIcon} alt="Delete" />
-                  </a>
-                </span>
-              </li>
-              <li className="App-list-item" key={2}>
-                <Link to="/something" className="App-routes-link">
-                  <span>
-                    <img
-                      className="App-routes-icon"
-                      src={routeIcon}
-                      width="50px"
-                      alt="Route icon"
-                    />
-                  </span>
-                  <span className="App-list-text">
-                    Origin point<br />Destination point
-                  </span>
-                </Link>
-                <span className="App-delete">
-                  <a>
-                    <img src={deleteIcon} alt="Delete" />
-                  </a>
-                </span>
-              </li>
-            </ul>
-          </div>
-        </main>
+                </li>
+              </ul>
+            </div>
+          </main>
+        )}
       </div>
     );
   }
