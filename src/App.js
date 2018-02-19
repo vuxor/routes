aeddc57f9bb8953a88ed90946ac031d4d3e3a8b6
Ivan Radeljic <radeljicivan85@gmail.com>
2018-02-19 23:58:41 +0100

    Add styles for GO link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 import LocationInput from './LocationInput';
 import './App.css';
 import routeIcon from './ic_near_me_black_24px.svg';
-import deleteIcon from './ic_close_black_24px.svg';
+import deleteIcon from './ic_close_red_24px.svg';
+import goIcon from './ic_arrow_forward_24px.svg';
 
 class App extends Component {
   render() {
@@ -26,11 +27,45 @@ class App extends Component {
               placeholderText="Enter your destination"
               placeChanged={place => console.log(place)}
             />
+            <Link to="/something" className="App-go">
+              <span>Go</span>
+              <svg
+                fill="#fff"
+                height="24"
+                viewBox="0 0 24 24"
+                width="24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M0 0h24v24H0z" fill="none" />
+                <path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z" />
+              </svg>
+            </Link>
           </div>
           <div className="App-list">
             <p className="App-intro">Your Previous Routes</p>
+            <hr />
             <ul>
-              <li className="App-list-item">
+              <li className="App-list-item" key={1}>
+                <Link to="/something" className="App-routes-link">
+                  <span>
+                    <img
+                      className="App-routes-icon"
+                      src={routeIcon}
+                      width="50px"
+                      alt="Route icon"
+                    />
+                  </span>
+                  <span className="App-list-text">
+                    Origin point<br />Destination point
+                  </span>
+                </Link>
+                <span className="App-delete">
+                  <a>
+                    <img src={deleteIcon} alt="Delete" />
+                  </a>
+                </span>
+              </li>
+              <li className="App-list-item" key={2}>
                 <Link to="/something" className="App-routes-link">
                   <span>
                     <img
