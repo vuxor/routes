5916fabb2bd3d6111b4f9214ed7c1ed6c2961d6f
Ivan Radeljic <radeljicivan85@gmail.com>
2018-02-20 13:11:21 +0100

    Add RouteDetail css file

--- src/RouteDetail.js
+++ src/RouteDetail.js
@@ -1,5 +1,17 @@
 import React from 'react';
 
-const RouteDetail = props => <div />;
+import MapWithRoute from './MapWithRoute';
+import './RouteDetail.css';
+
+const RouteDetail = props => (
+  <div className="RouteDetail">
+    <header>
+      <h1>Route Detail</h1>
+    </header>
+    <main className="RouteDetail-main">
+      <MapWithRoute />
+    </main>
+  </div>
+);
 
 export default RouteDetail;
