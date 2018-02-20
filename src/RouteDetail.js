b61d112ac5840c65eff1413e0a7dc7b6465bb6a9
Ivan Radeljic <radeljicivan85@gmail.com>
2018-02-20 12:48:51 +0100

    Add RouteDetail component

---
+++ src/RouteDetail.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const RouteDetail = props => <div />;
+
+export default RouteDetail;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'reset-css/reset.css';
 
 import './index.css';
 import App from './App';
+import RouteDetail from './RouteDetail';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <Router>
-    <Route exact path="/" component={App} />
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/route/:id" component={RouteDetail} />
+    </Switch>
   </Router>,
   document.getElementById('root')
 );
