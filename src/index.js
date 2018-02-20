a57cd9337b274cf7427e1a238e78b3282b41187c
Ivan Radeljic <radeljicivan85@gmail.com>
2018-02-20 08:20:15 +0100

    Add reset css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'reset-css/reset.css';
 
 import './index.css';
 import App from './App';
