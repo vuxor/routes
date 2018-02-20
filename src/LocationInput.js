dc2b1aaa580cb0b7f0389564c60ed3c471e92a71
Ivan Radeljic <radeljicivan85@gmail.com>
2018-02-20 17:41:10 +0100

    Add google maps script in head of html file

--- src/LocationInput.js
+++ src/LocationInput.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { compose, withProps, lifecycle } from 'recompose';
-import { withScriptjs } from 'react-google-maps';
 import { StandaloneSearchBox } from 'react-google-maps/lib/components/places/StandaloneSearchBox';
 
 import './LocationInput.css';
 
 const LocationInput = compose(
   withProps({
-    googleMapURL:
-      'https://maps.googleapis.com/maps/api/js?key=AIzaSyCWT2mBKoiV053AOTBLsiSiFIDD1S06pt0&v=3.exp&libraries=places',
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />
   }),
@@ -23,8 +20,7 @@ const LocationInput = compose(
         onPlacesChanged: () => refs.searchBox.getPlaces()
       });
     }
-  }),
-  withScriptjs
+  })
 )(props => (
   <div data-standalone-searchbox="" className="LocationInput">
     <StandaloneSearchBox
